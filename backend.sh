047481d7497f80452f729e9e91a083e5f8ebc4e7
vinaydms <vinay.dms16@gmail.com>
2026-06-07 13:46:55 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -26,7 +26,7 @@ fi
 mkdir -p /app &>> $LOGFILE
 
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>> $LOGFILE
 
 
 cd /app
