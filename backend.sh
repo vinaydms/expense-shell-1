7aad43e000981fa3d9fe940941e55bc057c206bd
vinaydms <vinay.dms16@gmail.com>
2026-06-07 13:41:31 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -6,25 +6,24 @@ check_root
 echo "please enter DB Password:"
 read -s mysql_root_password
 
-dnf module disable nodejs -y &>>LOGFILE
+dnf module disable nodejs -y &>> $LOGFILE
 
 
-dnf module enable nodejs:20 -y &>>LOGFILE
+dnf module enable nodejs:20 -y &>> $LOGFILE
 
 
-dnf install nodejs -y &>>LOGFILE
+dnf install nodejs -y &>> $LOGFILE
 
-
-id expense &>>LOGFILE
+id expense &>> $LOGFILE
 if [ $? -ne 0 ]
 then
-    useradd expense &>>LOGFILE
+    useradd expense &>> $LOGFILE
     
 else
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
-mkdir -p /app &>>LOGFILE
+mkdir -p /app &>> $LOGFILE
 
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
@@ -32,31 +31,31 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 
 cd /app
 rm -rf /app/*
-unzip /tmp/backend.zip &>>LOGFILE
+unzip /tmp/backend.zip &>> $LOGFILE
 
 
-npm install &>>LOGFILE
+npm install &>> $LOGFILE
 
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
 
 
-systemctl daemon-reload &>>LOGFILE
+systemctl daemon-reload &>> $LOGFILE
 
 
-systemctl start backend &>>LOGFILE
+systemctl start backend &>> $LOGFILE
 
 
-systemctl enable backend &>>LOGFILE
+systemctl enable backend &>> $LOGFILE
 
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>> $LOGFILE
 
 
-mysql -h db.devopsvinay.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>LOGFILE
+mysql -h db.devopsvinay.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>> LOGFILE
 
 
-systemctl restart backend &>>LOGFILE
+systemctl restart backend &>> $LOGFILE
 
 
 
