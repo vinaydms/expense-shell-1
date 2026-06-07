85aa5265301e5d1cf280def7483b115f4f70e7ae
vinaydms <vinay.dms16@gmail.com>
2026-06-07 14:35:20 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -59,13 +59,13 @@ read -s mysql_root_password
 # systemctl restart backend &>>$LOGFILE
 
 dnf module disable nodejs -y &>>$LOGFILE
-#VALIDATE $? "Disabling nodejs"
+VALIDATE $? "Disabling nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-#VALIDATE $? "Enabling nodejs"
+VALIDATE $? "Enabling nodejs"
 
 dnf install nodejs -y &>>$LOGFILE
-#VALIDATE $? "Installing nodejs"
+VALIDATE $? "Installing nodejs"
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
@@ -77,39 +77,39 @@ else
 fi
 
 mkdir -p /app &>>$LOGFILE
-#VALIDATE $? "Creating app directory"
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
-#VALIDATE $? "Downloading backend code"
+VALIDATE $? "Downloading backend code"
 
 cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-#VALIDATE $? "Extracted backend code"
+VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-#VALIDATE $? "Installing nodejs dependencies"
+VALIDATE $? "Installing nodejs dependencies"
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-#VALIDATE $? "Copied backend service"
+VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-#VALIDATE $? "Daemon reload"
+VALIDATE $? "Daemon reload"
 
 systemctl start backend &>>$LOGFILE
-#VALIDATE $? "Starting backend"
+VALIDATE $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-#VALIDATE $? "Enabling backend"
+VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-#VALIDATE $? "Installing Mysql client"
+VALIDATE $? "Installing Mysql client"
 
 mysql -h db.devopsvinay.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-#VALIDATE $? "Schema loading"
+VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-#VALIDATE $? "Restart backend"
+VALIDATE $? "Restart backend"
 
 
 
