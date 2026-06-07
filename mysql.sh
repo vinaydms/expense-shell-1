840adbfe0e088ca292afd94da418c1bc32f5fcaa
vinaydms <vinay.dms16@gmail.com>
2026-06-07 12:29:08 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -8,13 +8,13 @@ echo "please enter DB Password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-#VALIDATE $? "Installing MySQL Server"
+
 
 systemctl enable mysqld &>>$LOGFILE
-#VALIDATE $? "Enabling Mysql server"
+
 
 systemctl start mysqld &>>$LOGFILE
-#VALIDATE $? "Starting Mysql server"
+
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
 # VALIDATE $? "Setting up root Password"
@@ -24,7 +24,7 @@ systemctl start mysqld &>>$LOGFILE
  if [ $? -ne 0 ]
  then 
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    #VALIDATE $? "MYSQL Root Password Setup"
+   
 else 
     echo -e "Mysql Root Password is already setup...$Y SKIPPING $N"
 fi
